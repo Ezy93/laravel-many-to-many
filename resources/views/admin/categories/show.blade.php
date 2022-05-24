ff97e7d75315e0a981c31e3ce7c822e7bf248d2b
Ezy93 <giordani001@gmail.com>
2022-05-24 17:42:32 +0200

    create blade(da modifiare perche copiate da quelle dei posts) per le categorie e il controller

---
+++ resources/views/admin/categories/show.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                @if (session('message'))
+                    <div class="alert alert-success">{{session('message')}}</div>
+                @endif
+            </div>
+            <div class="col-12">
+                <div class="card">
+                    <img src="{{$post->img_url}}" class="card-img-top" alt="{{'immagine di '.$post->title}} ">
+                    <div class="card-body">
+                        <h3 class="card-title mb-3">{{$post->title}}</h3>
+                        <h5 class="card-subtitle mb-3">{{$post->author}}</h5>
+                        <p class="card-text">{{$post->description}}</p>
+                        <div class="d-flex">
+                            <a href="{{route('admin.posts.edit', $post->id)}}" class="btn btn-warning me-2">Edit</a>
+                            <form action="{{route('admin.posts.destroy', $post)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="delete" class="btn btn-danger">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
